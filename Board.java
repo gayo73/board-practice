5c7f3fb4e23d8c87e0f96993f3f42d0e6d98173c
gayo73 <gayoung2037@gmail.com>
2026-06-19 09:31:49 +0900

    feat: A updated Board.java

--- Board.java
+++ Board.java
@@ -1,10 +1,13 @@
+
+// [A, conflict/A add]
 public class Board {
     private int    id;
     private String title;
     private String author;
     // getter/setter 생략
-}
+
     // feature/A: 조회수 기능 추가
     private int viewCount = 0;
     public int  getViewCount()         { return viewCount; }
     public void incrementViewCount()   { this.viewCount++; }
+}
